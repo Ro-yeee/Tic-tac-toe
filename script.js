d0eef06caef50b0d15223583dfb82a891ac4991a
Roy Joseph <royjoseph.mec@gmail.com>
2023-06-23 17:20:01 +0530

    Added a restart Button

--- script.js
+++ script.js
@@ -16,7 +16,9 @@ const gameBoard = (() =>{
         board[index] = sign
     }
 
-    const getField = (index) => board[index]
+    const getField = (index) => {
+        return board[index]
+    }
 
     const reset = () =>{
         for(let i=0;i < board.length;i++){
@@ -29,14 +31,15 @@ const gameBoard = (() =>{
 
 // Module for the UI of the game
 const displayController = (() =>{
-    let round = 0
 
+    let round = 0
     const fields = document.querySelectorAll(".field")
     const messagefield = document.querySelector(".message")
+    const restartBtn = document.querySelector(".restartBtn")
 
     fields.forEach(field =>{
         field.addEventListener('click', (e) =>{
-            if(gameController.isFieldOccupied(e.target.dataset.index)) return
+            if(gameController.isFieldOccupied(e.target.dataset.index)|| gameController.getIsOver()) return
             gameController.playRound(e.target.dataset.index)
             field.textContent = gameBoard.getField(e.target.dataset.index)
             if(round % 2 !== 0){
@@ -52,18 +55,30 @@ const displayController = (() =>{
         messagefield.textContent = message
     }
 
-    const reset = () =>{
-        for(let i=0; i < fields.length; i++){
-            fields[i].textContent = ""
-        }
-    }
-
-    const setColor = (val) =>{
-        val.forEach(value =>{
+    const setWinColor = (winCombination) =>{
+        winCombination.forEach(value =>{
             fields[value].classList.add("yellow")
         })
     }
-    return{setMessage,reset,setColor}
+
+    const reset = () =>{
+        for(let i=0; i < fields.length; i++){
+            fields[i].textContent = ""
+            fields[i].classList.remove("yellow")
+            fields[i].classList.remove("white")
+            fields[i].classList.remove("pink")
+        }
+        setMessage("Player X's Turn")
+    }
+
+    restartBtn.addEventListener('click', () =>{
+        reset()
+        gameBoard.reset()
+        gameController.reset()
+        round = 0
+    })
+
+    return{setMessage,setWinColor}
 })()
 
 // Module for Game Controller , it controls the flow of the game
@@ -71,18 +86,21 @@ const gameController = (() =>{
     const player1 = Player("X")
     const player2 = Player("O")
     let round = 1
-    let isover = false
+    let isOver = false
 
     const playRound = (index) =>{
         gameBoard.setField(index, getCurrentPlayerSign())
         const winCombination = checkForWinner(index)
         if(winCombination){
             displayController.setMessage(`Player ${getCurrentPlayerSign()} Won !`)
-            displayController.setColor(winCombination)
+            displayController.setWinColor(winCombination)
+            isOver = true
             return
         }
         if(round === 9){
             displayController.setMessage(`It's a Draw`)
+            displayController.setWinColor([0,1,2,3,4,5,6,7,8])
+            isOver = true
             return
         }
         round++
@@ -103,18 +121,22 @@ const gameController = (() =>{
         let winCombination = false
         const winningCombinations = [ [0,1,2], [3,4,5], [6,7,8], [0,3,6], [1,4,7], [2,5,8], [0,4,8], [2,4,6] ]
         const combinationsToCheck = winningCombinations.filter(element => element.includes(fieldindex))
-        winningCombinations.some(combination => combination.every(element => gameBoard.getField(element) === getCurrentPlayerSign())? winCombination = combination:false)
-        return winCombination
+        combinationsToCheck.some(combination => combination.every(element => gameBoard.getField(element) === getCurrentPlayerSign())? winCombination = combination:false)
+        return winCombination // returns either the winning combination or if not present, then returns falsew
     }
 
     const reset = () =>{
         round = 1
-        isover = false
+        isOver = false
     }
 
     const isFieldOccupied = (index) =>{
         return gameBoard.getField(index) !== "" ? true : false
     }
 
-    return {playRound,reset,isFieldOccupied} 
+    const getIsOver = () =>{
+        return isOver
+    }
+
+    return {playRound,reset,isFieldOccupied,getIsOver} 
 })()
