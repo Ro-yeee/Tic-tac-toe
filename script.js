816dfb5fbf66dec6e0bb81f81cea4c1b8d076b80
Roy Joseph <royjoseph.mec@gmail.com>
2023-06-23 18:17:45 +0530

    Changed some spelling error

--- script.js
+++ script.js
@@ -122,7 +122,7 @@ const gameController = (() =>{
         const winningCombinations = [ [0,1,2], [3,4,5], [6,7,8], [0,3,6], [1,4,7], [2,5,8], [0,4,8], [2,4,6] ]
         const combinationsToCheck = winningCombinations.filter(element => element.includes(fieldindex))
         combinationsToCheck.some(combination => combination.every(element => gameBoard.getField(element) === getCurrentPlayerSign())? winCombination = combination:false)
-        return winCombination // returns either the winning combination or if not present, then returns falsew
+        return winCombination // returns either the winning combination or if not present, then returns false
     }
 
     const reset = () =>{
